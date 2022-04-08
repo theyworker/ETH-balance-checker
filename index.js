8f7a74b075adfd760196702a16ac5d7473d5ef24
Devaka <devaka.kumarasinghe@gmail.com>
2022-04-09 01:21:26 +0530

    clean code

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const { ethers } = require("ethers");
 const { infuraApiKey } = require("./PrivateKeys/infuraKey");
 
-
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${infuraApiKey}`)
+const provider = new ethers.providers.JsonRpcProvider(
+  `https://mainnet.infura.io/v3/${infuraApiKey}`
+);
 
 const main = async () => {
+  const balance = await provider.getBalance(
+    "0x7CFB6A3Cb07b9F9B627ee2c0935F3A3FB71E3e1F"
+  );
 
-    const balance = await provider.getBalance('0x7CFB6A3Cb07b9F9B627ee2c0935F3A3FB71E3e1F');
-    
-    console.log('Balance : ', ethers.utils.formatEther(balance));
-}
+  console.log("Balance : ", ethers.utils.formatEther(balance));
+};
 
-main()
+main();
