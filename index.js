13adfb41c03b191a1bdca30ed7bb850290d50123
Devaka <devaka.kumarasinghe@gmail.com>
2022-04-09 01:17:31 +0530

    first

---
+++ index.js
@@ -0,0 +1,14 @@
+const { ethers } = require("ethers");
+const { infuraApiKey } = require("./PrivateKeys/infuraKey");
+
+
+const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${infuraApiKey}`)
+
+const main = async () => {
+
+    const balance = await provider.getBalance('0x7CFB6A3Cb07b9F9B627ee2c0935F3A3FB71E3e1F');
+    
+    console.log('Balance : ', ethers.utils.formatEther(balance));
+}
+
+main()
